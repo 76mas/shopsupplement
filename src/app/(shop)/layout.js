6ccfa9872b2247abf760b9c6ad7e9747d7b1a0f1
MAS <mhmwdbas310@gmail.com>
2026-05-08 15:43:53 +0300

    Update layout.js

--- src/app/(shop)/layout.js
+++ src/app/(shop)/layout.js
@@ -2,12 +2,15 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { CartProvider } from "@/context/cart-context";
 import CartDrawer from "@/components/cart/cart-drawer";
+import { Suspense } from "react";
 
 export default function ShopLayout({ children }) {
   return (
     <CartProvider>
       <div className="min-h-full flex flex-col">
-        <Header />
+        <Suspense fallback={<div className="h-20 bg-white" />}>
+          <Header />
+        </Suspense>
         <main className="flex-grow">
           {children}
         </main>
