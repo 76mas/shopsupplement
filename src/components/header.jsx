499c0426ad1166444e153a46239712c0a20f1ff9
MAS <mhmwdbas310@gmail.com>
2026-05-29 00:11:31 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -137,9 +137,10 @@ const Header = () => {
                     <path d="M18 6 6 18"/><path d="m6 6 12 12"/>
                   </svg>
                 ) : (
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" />
-                  </svg>
+                  // <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  //   <circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" />
+                  // </svg>
+                  <Search />
                 )}
               </button>
 
